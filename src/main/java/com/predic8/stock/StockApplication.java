259ae2a7d2f719392bee2465a81360919f309193
Andreas Maier <a.maier@systems62.com>
2021-04-14 11:37:16 +0200

    changes day 3

--- src/main/java/com/predic8/stock/StockApplication.java
+++ src/main/java/com/predic8/stock/StockApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.beans.Transient;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
